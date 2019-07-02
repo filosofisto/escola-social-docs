1f58f77c58cd19023b1c975ac8cd6ea93525e4a5
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-07-01 23:20:24 -0300

    Atualização do script de banco, incluindo DESPESAS

--- script.sql
+++ script.sql
@@ -15,7 +15,34 @@ CREATE SCHEMA escola_social
 ALTER DEFAULT PRIVILEGES IN SCHEMA escola_social
 GRANT SELECT ON TABLES TO escola_social_dev;
 
-  -- Table: escola_social.absenteismo
+-- SEQUENCE: escola_social.absenteismo_id_seq
+
+-- DROP SEQUENCE escola_social.absenteismo_id_seq;
+
+CREATE SEQUENCE escola_social.absenteismo_id_seq;
+
+ALTER SEQUENCE escola_social.absenteismo_id_seq
+    OWNER TO escola_social_dev;
+
+-- SEQUENCE: escola_social.assiduidade_id_assiduidade_seq
+
+-- DROP SEQUENCE escola_social.assiduidade_id_assiduidade_seq;
+
+CREATE SEQUENCE escola_social.assiduidade_id_assiduidade_seq;
+
+ALTER SEQUENCE escola_social.assiduidade_id_assiduidade_seq
+    OWNER TO escola_social_dev;
+
+-- SEQUENCE: escola_social.quadro_despesa_id_seq
+
+-- DROP SEQUENCE escola_social.quadro_despesa_id_seq;
+
+CREATE SEQUENCE escola_social.quadro_despesa_id_seq;
+
+ALTER SEQUENCE escola_social.quadro_despesa_id_seq
+    OWNER TO escola_social_dev;
+
+-- Table: escola_social.absenteismo
 
 -- DROP TABLE escola_social.absenteismo;
 
@@ -76,3 +103,50 @@ CREATE UNIQUE INDEX idx_assiduidade_idsic
     ON escola_social.assiduidade USING btree
     (id_sic)
     TABLESPACE pg_default;
+
+-- Table: escola_social.quadro_despesa
+
+-- DROP TABLE escola_social.quadro_despesa;
+
+CREATE TABLE escola_social.quadro_despesa
+(
+    id integer NOT NULL DEFAULT nextval('escola_social.quadro_despesa_id_seq'::regclass),
+    ano integer NOT NULL,
+    mes integer NOT NULL,
+    semana integer NOT NULL,
+    lei double precision,
+    alteracao_lei double precision,
+    percentual_lei numeric(5,2),
+    contigenciado double precision,
+    percentual_contigenciado numeric(5,2),
+    cota double precision,
+    percentual_cota numeric(5,2),
+    bloqueado double precision,
+    despesa_autorizada double precision,
+    empenhado double precision,
+    disponivel double precision,
+    percentual_disponivel numeric(5,2),
+    liquidado double precision,
+    percentual_liquidado numeric(5,2)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE escola_social.quadro_despesa
+    OWNER to escola_social_dev;
+
+-- Index: idx_quadro_despesa_ano_mes_semana
+
+-- DROP INDEX escola_social.idx_quadro_despesa_ano_mes_semana;
+
+CREATE UNIQUE INDEX idx_quadro_despesa_ano_mes_semana
+    ON escola_social.quadro_despesa USING btree
+    (ano, mes, semana)
+    TABLESPACE pg_default;
+
+-- Change user password
+ALTER USER escola_social_dev WITH PASSWORD '12345678';
+
+    
